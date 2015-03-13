deb570049aedae0de5dff085d18830e73694cc73
Josh Levinger <josh@levinger.net>
2015-03-12 19:02:53 -0700

    hide introModal after dismissing once

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,8 +12,20 @@ window.Solidarity = {
 
         // new this.Views.storyMap({ });
 
-        // show demo modal
-        $('#loadModal').modal('show');
+        // show intro modal only on first load
+        if($.cookie('hideModal') === 'true') {
+            // already saw modal, don't show again
+            $('#introModal').modal('hide');
+        } else {
+            $('#introModal').modal('show');
+
+        }
+
+        $('#introModal').on('hidden.bs.modal', function () {
+            console.log('hide modal');
+            $.cookie('hideModal', 'true');
+        });
+        
     }
 };
 
