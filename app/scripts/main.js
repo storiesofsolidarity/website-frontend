a0dadf1108f24a5ecf41116c5d362a2294177872
Josh Levinger <josh@levinger.net>
2015-03-17 20:03:05 -0700

    missing global urlParam

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,7 +22,19 @@ window.Solidarity = {
 
         // start history
         Backbone.history.start();
-    }
+    },
+
+    urlParam: function(name) {
+        // get a named parameter from the location hash
+        // have to use regex b/c it's not in window.location.search
+        var hashMatch = window.location.href.match(/^[^#]+#([^?]*)\??(.*)/);
+        var hashPath = hashMatch[1];
+        var hashQuery = hashMatch[2];
+
+        // simplest solution using splits, but may not handle multiple param names
+        var half = hashQuery.split(name + '=')[1];
+        return half !== undefined ? decodeURIComponent(half.split('&')[0]) : null;
+    },
 };
 
 $(document).ready(function () {
