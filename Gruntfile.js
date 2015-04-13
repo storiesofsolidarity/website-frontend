7eccbea1839ca9104176708e87e1fb610206acd4
Josh Levinger <josh@levinger.net>
2015-04-13 16:23:43 -0700

    grunt check js,json for images to rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,8 +171,16 @@ module.exports = function (grunt) {
         usemin: {
             html: ['<%= yeoman.dist %>/{,*/}*.html'],
             css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+            js: '<%= yeoman.dist %>/scripts/{,*/}*.{js,json}',
             options: {
-                dirs: ['<%= yeoman.dist %>']
+                dirs: ['<%= yeoman.dist %>'],
+                assetsDirs: ['<%= yeoman.dist %>', '<%= yeoman.dist %>/images'],
+                patterns: {
+                  // FIXME While usemin won't have full support for revved files we have to put all references manually here
+                  js: [
+                      [/(images\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']
+                  ]
+                }
             }
         },
         imagemin: {
@@ -225,7 +233,7 @@ module.exports = function (grunt) {
                     dest: '<%= yeoman.dist %>',
                     src: [
                         '*.{ico,txt}',
-                        'images/{,*/}*.{webp,gif}',
+                        'images/{,*/}*.{webp,gif,png,jpg}',
                         'styles/fonts/{,*/}*.*',
                         'scripts/map/*.json',
                         'scripts/splash/*.json',
