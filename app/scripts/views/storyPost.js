e5b186fe5910cebac09230bf9d13d27888ba6e90
Josh Levinger <josh@levinger.net>
2015-04-14 16:42:36 -0700

    storyPost geolocation

--- app/scripts/views/storyPost.js
+++ app/scripts/views/storyPost.js
@@ -1,4 +1,4 @@
-/*global Solidarity, Backbone, JST*/
+/*global Solidarity, Backbone, JST, Modernizr*/
 
 Solidarity.Views = Solidarity.Views || {};
 
@@ -10,6 +10,7 @@ Solidarity.Views = Solidarity.Views || {};
         template: JST['app/scripts/templates/storyPost.ejs'],
         el: '#content',
         form: 'form#storyPost',
+        events: {'click button#geolocate': 'geolocate'},
         
         render: function () {
             this.$el.html(this.template());
@@ -24,6 +25,49 @@ Solidarity.Views = Solidarity.Views || {};
             }); 
             $('.selectpicker').selectpicker();
             return this;
+        },
+
+        geolocate: function() {
+            if (Modernizr.geolocation) {
+                console.log('geolocate!');
+                navigator.geolocation.getCurrentPosition(this.locationToCityState, this.geoError.bind(this));
+            } else {
+                return this.geoError({code: 0, message: 'Your browser does not support geolocation.'});
+            }
+        },
+
+        geoError: function(error) {
+            this.clearAlerts();
+
+            var message = 'Geolocation error';
+            if (error.code === 1) { message = 'Please allow the browser to access your location.'; }
+            if (error.code === 2) { message = 'Geolocation position unavailable.'; }
+            if (error.code === 3) { message = 'Geolocation position timeout.'; }
+
+            this.$form.append(this.templateError({'non_field_errors': message}));
+        },
+
+        locationToCityState: function(position) {
+            var self = this;
+
+            $.ajax('http://nominatim.openstreetmap.org/reverse?format=json', {
+                type: 'GET',
+                data: {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                },
+                success: function(data) {
+                    $('input#city').val(data.address.city);
+                    var stateVal = $('select#state option')
+                        .filter(function(i, e) { return $(e).text() === data.address.state; } )
+                        .val(); //lookup state val from name
+                    $('#state.selectpicker').selectpicker('val', stateVal);
+                    $('button#geolocate').attr('disabled','disabled');
+                },
+                error: function(resp, status, err) {
+                    return self.geoError({code: 4, message: err});
+                }
+            });
         }
 
     });
