d945b8209e949620b566e8aba21ab0069b2e98c3
Josh Levinger <josh@levinger.net>
2015-09-30 13:16:15 -0700

    splash transitions WIP

--- app/scripts/views/splash.js
+++ app/scripts/views/splash.js
@@ -1,4 +1,4 @@
-/*global Solidarity, Backbone, JST*/
+/*global Solidarity, Backbone, JST, jquery*/
 
 Solidarity.Views = Solidarity.Views || {};
 
@@ -11,22 +11,63 @@ Solidarity.Views = Solidarity.Views || {};
         el: '#content',
         events: {
             'click button.enter': 'enter',
-            'mouseover button.lang': 'language',
+            'click button.lang': 'clickLanguage',
         },
         languages: ['zh', 'en', 'es', 'tl'], // defined in display order
+        langIndex: 0,
+        rotateLanguageTimer: undefined,
 
-        initialize: function() {
-            // start rotate timer
+        render: function () {
+            this.$el.html(this.template());
+
+            // start timers bound to view
+            this.rotateLanguageTimer = setInterval(this.rotateLanguage.bind(this), 2.5*1000);
+            
+            // set up background image switcher
+            $('.splash').bgswitcher({
+                images: ['/images/splash/temp-1.jpg',
+                         '/images/splash/temp-2.jpg',
+                         '/images/splash/temp-3.jpg'],
+                interval: 10*1000,
+                effect: 'fade',
+            }).bgswitcher('start');
+
+            return this;
         },
 
-        rotate: function() {
+        rotateLanguage: function() {
+            this.langIndex += 1;
+            if (this.langIndex === this.languages.length) { this.langIndex = 0; }
 
+            var nextLanguage = this.languages[this.langIndex];
+            var btn = $('ul.languages .btn[data-lang='+nextLanguage+']');
+            return this.changeLanguage(btn);
         },
 
-        language: function(event) {
-            var lang = $(event.target).data('lang');
+        changeLanguage: function(btn) {
+            var lang = btn.data('lang');
+            $('ul.languages .btn.active').removeClass('active');
             $('ul.statement li').hide();
             $('ul.statement li#'+lang).show();
+            btn.addClass('active');
+        },
+
+        clickLanguage: function(event) {
+            var target = $(event.target);
+            var btn = target;
+            if (btn.is('span')) {
+                btn = target.parents('button');
+            }
+            // stop rotation timer
+            clearInterval(this.rotateLanguageTimer);
+
+            return this.changeLanguage(btn);
+        },
+
+        rotateImage: function() {
+            this.imageIndex += 1;
+            if (this.imageIndex === this.images.length) { this.imageIndex = 0; }
+            $('div.splash').css('background-image', 'url('+this.images[this.imageIndex]+')');
         },
 
         enter: function() {
